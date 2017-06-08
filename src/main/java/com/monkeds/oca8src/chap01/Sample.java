40aa135585f22a1c41faec81eba113d7bd3b6df3
monked <monked@MacBook-Pro-de-monked.local>
2017-06-07 22:58:30 -0500

    second commit

---
+++ src/main/java/com/monkeds/oca8src/chap01/Sample.java
@@ -0,0 +1,11 @@
+package com.monkeds.oca8src.chap01;
+
+/**
+ * Created by monked on 7/06/17.
+ */
+public class Sample {
+
+    public static void main (String... args){
+        System.out.println("Hola");
+    }
+}
